fffe76a074f189f0de74ac28954c6f334f7b649e
alfredosource <alfred@alfredosource.com>
2011-03-16 09:16:54 -0400

    Finished Layout and Routes, ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp1::Application.routes.draw do
+  get "users/new"
+  match '/signup', :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
